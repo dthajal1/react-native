1fb31bc7831b72b4124737460ccfca9009a25add
Diraj Thajali <thajalidiraj@gmail.com>
2021-11-18 21:06:12 -0800

    first proj mac

---
+++ shoppingListMac/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>Hello world From Mac!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  txt: {
+    fontSize: 20
+  }
+})
+
+export default App;
